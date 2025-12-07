ea1538f0b36c0fe72827542d06666de439b1fa0c
mrktguru <gommeux@gmail.com>
2025-12-07 21:12:43 +0200

    Fix indentation: correct account ID generation block

--- web_app.py
+++ web_app.py
@@ -1160,9 +1160,9 @@ def new_campaign():
             account = next((acc for acc in all_accounts if acc.get('phone') == phone), None)
             if account:
                 account_id = account.get('id')
-                    # Generate new ID: acc_{phone}_{campaign_id}
+                # Generate new ID: acc_{phone}_{campaign_id}
                 phone_clean = phone.replace('+', '').replace(' ', '').replace('-', '')
-                    new_account_id = f"acc_{phone_clean}_{campaign_id}"
+                new_account_id = f"acc_{phone_clean}_{campaign_id}"
                     
                     # Update account with new ID and campaign_id
                 update_account(account_id, {
